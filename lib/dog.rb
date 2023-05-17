763645fc3fee384ecbe6e90ea586aca4a465c388
iamkinglana <ngetichenock32@gmail.com>
2023-05-17 23:21:20 +0300

    First Commit

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    attr_accessor :name, :breed
+    def initialize(name,breed="Mutt")
+        @name = name 
+        @breed = breed       
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+end
+
